d797104212abcc85c39b3eb08c0227bfcf49755b
Hellen02Henandez2021 <87906169+Hellen02Henandez2021@users.noreply.github.com>
2021-11-13 11:04:17 -0600

    primer commit

---
+++ ejer car 2 parcial/logica.py
@@ -0,0 +1,7 @@
+class Numeros:
+    a=int(input("ingrese un numero"))
+    b=int(input("ingrese un numero"))
+    if a>b:
+        print("el mayor valor es a")
+     if a<b:
+         print("el mayor valor es b")
